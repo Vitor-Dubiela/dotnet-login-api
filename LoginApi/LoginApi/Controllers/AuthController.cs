b050441c5e64da8b9952d799be198291b991e7b3
Vitor Hugo Santos Dubiela <fu@Vitors-MacBook-Air.local>
2023-02-22 07:47:53 -0300

    Bootstrap LoginAPI

---
+++ LoginApi/LoginApi/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LoginApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginApi.Controllers
+{
+	[Route("api/auth")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		public static List<User> _users = new List<User>();
+
+		[HttpPost("register-user")]
+		public async Task<ActionResult<User>> RegisterUser([FromBody] UserDto userDto)
+		{
+			if (!ValidateUsername(userDto.Username))
+				return BadRequest("The User name was not valid.");
+
+			return Ok();
+		}
+
+		private static bool ValidateUsername(string username)
+		{
+			foreach (var user in _users)
+			{
+				if (user.Username.Equals(username))
+					return false;
+			}
+			return true;
+		}
+	}
+}
+
---
+++ LoginApi/LoginApi/Models/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LoginApi.Models
+{
+	public class UserDto
+	{
+		public required string Username { get; set; } = string.Empty;
+		public required string Password { get; set; } = string.Empty;
+	}
+}
+
