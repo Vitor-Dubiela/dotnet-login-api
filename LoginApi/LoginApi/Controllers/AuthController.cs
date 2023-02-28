56c91bf79b17ae8933493db22b33e165f52a68e0
Vitor Hugo Santos Dubiela <fu@Vitors-MacBook-Air.local>
2023-02-27 22:03:08 -0300

    Add CreateToken Method
    Added the method responsible for creating the JWT, for the user login authentication.

--- LoginApi/LoginApi/Controllers/AuthController.cs
+++ LoginApi/LoginApi/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using LoginApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LoginApi.Controllers
 {
@@ -9,6 +13,12 @@ namespace LoginApi.Controllers
     public class AuthController : ControllerBase
     {
         public static User _user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register-user")]
         public ActionResult<User> RegisterUser([FromBody] UserDto userDto)
@@ -32,10 +42,37 @@ namespace LoginApi.Controllers
         [HttpPost("login")]
         public ActionResult<User> Login([FromBody] UserDto userDto)
         {
+            if (!userDto.Username.Equals(_user.Username))
+                return BadRequest("The User name is invalid.");
+
             if (!BCrypt.Net.BCrypt.Verify(userDto.Password, _user.PasswordHash))
                 return BadRequest("The password is invalid.");
 
-            return Ok("You are connected.");
+            var token = CreateToken(_user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Authentication:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
